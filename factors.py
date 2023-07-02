07f0f50b0fff61a6a341cb9944fc10adaafdb8d1
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-02 15:57:56 -0700

    Factorize all the things!

--- factors.py
+++ factors.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 import sys
+import subprocess
 
 
-def factor(buffer):
-    num = int(buffer)
-    for i in range(2, num):
-        if num % i == 0:
-            print(f"{num} = {num // i} * {i}")
-            break
+def check_factor(args):
+    if len(args) != 3:
+        count = 0
+        num2 = 1
+        for a in args:
+            if count > 1:
+                num2 *= int(a)
+            count += 1
+    else:
+        num2 = int(args[2])
+    num1 = int(args[1])
+    num = args[0].replace(':', '=')
+    result = 1 if num2 > num1 else 0
+    if result == 1:
+        numcp = num1
+        num1 = num2
+        num2 = numcp
+    print(f"{num}{num1}*{num2}")
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: factor <filename>")
-        sys.exit(1)
-    filename = sys.argv[1]
-    try:
-        with open(fiename, 'r') as file:
-            numbers = file.readlines()
-        for number in numbers:
-            foctor(number.script())
-    except I0Error:
-        sys.stderr.write(f"Error: can't open file {filename}\n")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: factors <file>")
+    sys.exit(1)
 
 
-if __name__ == '__main__':
-    main()
+filename = sys.argv[1]
+with open(filename, 'r') as file:
+    lines = file.readlines()
+for line in lines:
+    result = subprocess.check_output(['factor', line], universal_newlines=True)
+    check_factor(result.strip().split())
